1a8167d93d2831b501ab613cc0e874549ce6600a
hst2002 <hst0604@gmail.com>
2025-06-05 21:12:54 +0900

    add page move

--- frontend/vite-project/src/main.jsx
+++ frontend/vite-project/src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { ReactDom, createRoot } from 'react-dom/client'
+import { BrowserRouter} from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
 import Lobby from './lobby.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Lobby />
-  </StrictMode>,
-)
+    <App />
+  </StrictMode>
+);
